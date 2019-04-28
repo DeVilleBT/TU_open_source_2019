a05560c251412fbcf7c9e9e01c5dad6c220fe44b
DeVilleBT <48925169+DeVilleBT@users.noreply.github.com>
2019-04-28 13:18:59 +0200

    Add files via upload
    first try at a function

---
+++ firsttry.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 13:12:30 2019
+
+@author: David O
+"""
+def firsttry_fun( x, y):
+        out = x + y
+        return( out )
+                        
